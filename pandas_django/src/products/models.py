8b58a4101abc60f733aace45a6fb21feee9f860e
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-19 16:15:55 +0330

    add purchese model

--- pandas_django/src/products/models.py
+++ pandas_django/src/products/models.py
@@ -1,12 +1,28 @@
 from django.db import models
-
-
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=220)
     date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.name
+
+
+class Purchese(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quentity = models.PositiveIntegerField()
+    total_price = models.PositiveSmallIntegerField(blank=True)
+    salesman = models.ForeignKey(User, on_delete=models.CASCADE)
+    #TODO:  change required
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        self.total_price = self.price * self.quentity
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        return f"Solled {self.product.name} - {self.quentity} items for {self.total_price}"
     
     
