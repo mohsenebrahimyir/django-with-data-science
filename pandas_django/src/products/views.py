0bb14e95f8b5dd67b77510fe907959f38cb3d901
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-19 23:38:58 +0330

    displaying error message in html if df is empty

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -4,14 +4,24 @@ import pandas as pd
 
 
 def chart_select_view(request):
+    error_message = None
     product_df = pd.DataFrame(Product.objects.all().values())
-    rename_product_col = {col:"product_" + col for col in product_df.columns}
-    product_df = product_df.rename(rename_product_col, axis=1)
+    rename_col = {col:"product_" + col for col in product_df.columns}
+    product_df = product_df.rename(rename_col, axis=1)
     purchese_df = pd.DataFrame(Purchese.objects.all().values())
-    df = pd.merge(purchese_df, product_df, on="product_id").drop(['product_date'], axis=1)
+    
+    if purchese_df.size > 0:
+        df = pd.merge(purchese_df, product_df, on="product_id").drop(['product_date'], axis=1)
+        if request.method == "POST":
+            chart_type = request.POST.get('sales')
+            date_from = request.POST['date_from']
+            date_to = request.POST['date_to']
+    else:
+        error_message = "No records in database"
+        df = None
+    
     context = {
-        "products": product_df.to_html(),
-        "purchese": purchese_df.to_html(),
-        "dataframe": df.to_html(),
+        "error_message": error_message,
     }
+    
     return render(request, "products/main.html", context)
