6176c149a952c243828405e5aa36b3cc2765a948
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-19 17:36:48 +0330

    set view and urls for performance page

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -1,3 +1,17 @@
+from itertools import product
+from multiprocessing import context
+from turtle import pd
 from django.shortcuts import render
+from .models import Product, Purchese
+import pandas as pd
 
-# Create your views here.
+
+def chart_select_view(request):
+    products_df = pd.DataFrame(Product.objects.all().values())
+    purchese_df = pd.DataFrame(Purchese.objects.all().values())
+    
+    context = {
+        "products": products_df.to_html(),
+        "purchese": purchese_df.to_html(),
+    }
+    return render(request, "products/main.html", context)
