672e651ccf7be80ed574df7ef26e124d7319928d
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-19 19:23:23 +0330

    merging 2 dataframes

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -1,17 +1,17 @@
-from itertools import product
-from multiprocessing import context
-from turtle import pd
 from django.shortcuts import render
 from .models import Product, Purchese
 import pandas as pd
 
 
 def chart_select_view(request):
-    products_df = pd.DataFrame(Product.objects.all().values())
+    product_df = pd.DataFrame(Product.objects.all().values())
     purchese_df = pd.DataFrame(Purchese.objects.all().values())
-    
+    col_rename = {col:"product_" + col for col in product_df.columns}
+    product_df = product_df.rename(col_rename, axis=1)
+    df = pd.merge(product_df, purchese_df, on="product_id")
     context = {
-        "products": products_df.to_html(),
+        "products": product_df.to_html(),
         "purchese": purchese_df.to_html(),
+        "dataframe": df.to_html(),
     }
     return render(request, "products/main.html", context)
