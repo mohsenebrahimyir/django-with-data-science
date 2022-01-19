ce121bcb5d9238200241f0dec548a3c96faed6aa
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-19 19:33:27 +0330

    minor edit merge dataframes

--- pandas_django/src/products/views.py
+++ pandas_django/src/products/views.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 def chart_select_view(request):
     product_df = pd.DataFrame(Product.objects.all().values())
+    rename_product_col = {col:"product_" + col for col in product_df.columns}
+    product_df = product_df.rename(rename_product_col, axis=1)
     purchese_df = pd.DataFrame(Purchese.objects.all().values())
-    col_rename = {col:"product_" + col for col in product_df.columns}
-    product_df = product_df.rename(col_rename, axis=1)
-    df = pd.merge(product_df, purchese_df, on="product_id")
+    df = pd.merge(purchese_df, product_df, on="product_id").drop(['product_date'], axis=1)
     context = {
         "products": product_df.to_html(),
         "purchese": purchese_df.to_html(),
